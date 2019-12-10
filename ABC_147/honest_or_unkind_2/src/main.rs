5734b8be78922692541e331ad215022ffbdaba11
Ryunosuke Yamada <chihaya9373-adlt@yahoo.co.jp>
2019-12-10 22:44:08 +0900

    解答を参考にC問題を解答.

--- ABC_147/honest_or_unkind_2/src/main.rs
+++ ABC_147/honest_or_unkind_2/src/main.rs
@@ -11,30 +11,49 @@ fn read_vec<T: std::str::FromStr>() -> Vec<T> {
         .map(|e| e.parse().ok().unwrap()).collect()
 }
 
-// 標準入力からn行を読み取り、各行を空白文字で分割し、各要素を指定の型に変換する関数
-fn read_vec2<T: std::str::FromStr>(n: u32) -> Vec<Vec<T>> {
-    (0..n).map(|_| read_vec()).collect()
-}
+use std::cmp::*;
 
 fn main() {
-    let n: u32 = read();
-    // []1: i番目の証言者
-    // []2: 対象の証言者i(1から始まることに注意. つまりi + 1)
-    // []3: どんな人であるか 0:不親切 1:正直者
-    let mut a: Vec<Vec<Vec<u32>>> = Vec::new();
-    a.resize(n as usize, Vec::new());
-
+    let n: usize = read();
+    // 証言の配列
+    let mut testimonies= vec![vec![]; n];
     for index in 0..n
     {
-        let a_n: u32 = read();
-        for jndex in 0..a_n as usize
+        // 各人の証言を入力
+        let a_n: usize = read();
+        for _ in 0..a_n
         {
-            let x_y: Vec<u32> = read_vec();
-            a[index as usize].push(x_y);
+            let testimony: Vec<u32> = read_vec();
+            // タプルを突っ込む: .0=対象の人 .1=正直者か不親切な人か
+            testimonies[index].push((testimony[0] - 1, testimony[1]));
         }
     }
 
-    
+    // 正直者の最大人数
+    let mut honest_count_max: u32 = 0;
+    // 証言の検証 bit全探索
+    'outer: for bp in 0..1 << n
+    {
+        // 正直者である人物の証言を調べ、矛盾を確かめる.
+        for index in 0..n
+        {
+            // 正直者である場合、証言を調べる
+            if bp >> index & 1 == 1
+            {
+                for t_index in 0..testimonies[index].len()
+                {
+                    // 矛盾の発見
+                    if bp >> testimonies[index][t_index].0 & 1 != testimonies[index][t_index].1
+                    {
+                        continue 'outer;
+                    }
+                }
+            }
+        }
 
-    println!("{:?}", a);
+        // count_ones()はビット列中の'1'の数を返す
+        honest_count_max = max(honest_count_max, bp.count_ones());
+    }
+
+    println!("{}", honest_count_max);
 }
